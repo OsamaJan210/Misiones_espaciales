89a70cd0dbf8086ae56e9300ab30db49f485adca
Marc-0207 <a17marlamver@inspedralbes.cat>
2025-06-02 13:54:30 +0200

    Creacion menu

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,16 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 import enums.ExperienciaTipo;
 import enums.MissionType;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Simulador de Misiones Espaciales!");
+       // System.out.printf("Simulador de Misiones Espaciales!");
         List<NavesEspaciales> naves = new ArrayList<>();
         List<Mision> misiones = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
 
         NavesEspaciales nave1 = new NavesEspaciales("Omega", 1500, 1500, 700, true,0,0,0,0);
         NavesEspaciales nave2 = new NavesEspaciales("Alpha", 900, 900, 600, true,0,0,0,0);
@@ -25,8 +27,36 @@ public class Main {
         naves.add(nave1);
         naves.add(nave2);
         naves.add(nave3);
+        int opcion = 0;
+        do{
+            System.out.println("\n*****Bienvenido al simulador espacial*****\n\n¿Que deseas hacer?\n\n1)Registrar Mision\n2)Registar Nave\n3)Simular un ciclo\n4)Mostrar estado general\n5)Buscar Mision\n6)Ranking de naves\n7)Salir");
+            opcion = scanner.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    registarMision();
+                break;
+                case 2:
+                    registarNave();
+                break;
+                case 3:
+                    simularCiclo();
+                break;
+                case 4:
+                    mostrarEstado();
+                break;
+                case 5:
+                    buscarMisiones();
+                break;
+                case 6:
+                    generarRanking();
+                break;
+                case 7:
+                    System.out.println("Adios!");
+                break;
+                
+            }
+        }while (opcion!=7); 
+        }
         
-
-
-    }
 }
