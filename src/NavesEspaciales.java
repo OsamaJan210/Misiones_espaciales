296ea231f93526c7472d42f417fc3c762b817f0d
Marc-0207 <a17marlamver@inspedralbes.cat>
2025-06-02 11:39:52 +0200

    Creacion Naves Espaciales --Marc

--- src/NavesEspaciales.java
+++ src/NavesEspaciales.java
@@ -1,3 +1,61 @@
 public class NavesEspaciales {
     
+    private String nombre;
+    private int autonomiaMaxima;
+    private int autonomiaActual;
+    private int capacidadCarga;
+    private boolean sensoresCientificos;
+    private int experienciaTotal;
+    private int experienciaTecnica;
+    private int experienciaCientifica;
+    private int experienciaEstrategica;
+
+    public NavesEspaciales(String nombre, int autonomiaMaxima, int autonomiaActual, int capacidadCarga, boolean sensoresCientificos, int experienciaTotal, int experienciaTecnica, int experienciaCientifica, int experienciaEstrategica) {
+        this.nombre = nombre;
+        this.autonomiaMaxima = autonomiaMaxima;
+        this.autonomiaActual = autonomiaActual;
+        this.capacidadCarga = capacidadCarga;
+        this.sensoresCientificos = sensoresCientificos;
+        this.experienciaTotal = experienciaTotal;
+        this.experienciaTecnica = experienciaTecnica;
+        this.experienciaCientifica = experienciaCientifica;
+        this.experienciaEstrategica = experienciaEstrategica;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getAutonomiaMaxima() {
+        return autonomiaMaxima;
+    }
+
+    public int getAutonomiaActual() {
+        return autonomiaActual;
+    }
+
+    public int getCapacidadCarga() {
+        return capacidadCarga;
+    }
+
+    public boolean tieneSensoresCientificos() {
+        return sensoresCientificos;
+    }
+
+    public int getExperienciaTotal() {
+        return experienciaTotal;
+    }
+
+    public int getExperienciaTecnica() {
+        return experienciaTecnica;
+    }
+
+    public int getExperienciaCientifica() {
+        return experienciaCientifica;
+    }
+
+    public int getExperienciaEstrategica() {
+        return experienciaEstrategica;
+    }
+
 }
