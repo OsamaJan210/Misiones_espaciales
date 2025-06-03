b39d30a078e792f3a4badb9783aac18268addb37
Marc-0207 <a17marlamver@inspedralbes.cat>
2025-06-03 10:02:39 +0200

    RegistarMision casi funcional

--- src/Main.java
+++ src/Main.java
@@ -7,11 +7,12 @@ import enums.MissionType;
 
 public class Main {
     public static void main(String[] args) {
-        List<NavesEspaciales> naves = new ArrayList<>();
-        List<Mision> misiones = new ArrayList<>();
+        //List<NavesEspaciales> naves = new ArrayList<>();
+       // List<Mision> misiones = new ArrayList<>();
+        Mision misiones = new Mision(null, 0, 0, null, null);
         Scanner scanner = new Scanner(System.in);
 
-        NavesEspaciales nave1 = new NavesEspaciales("Omega", 1500, 1500, 700, true,0,0,0,0);
+        /*NavesEspaciales nave1 = new NavesEspaciales("Omega", 1500, 1500, 700, true,0,0,0,0);
         NavesEspaciales nave2 = new NavesEspaciales("Alpha", 900, 900, 600, true,0,0,0,0);
         NavesEspaciales nave3 = new NavesEspaciales("Beta", 1200, 1200, 400, true,0,0,0,0);
 
@@ -25,15 +26,15 @@ public class Main {
 
         naves.add(nave1);
         naves.add(nave2);
-        naves.add(nave3);
+        naves.add(nave3);*/
         int opcion = 0;
         do{
             System.out.println("\n*****Bienvenido al simulador espacial*****\n\n¿Que deseas hacer?\n\n1)Registrar Mision\n2)Registar Nave\n3)Simular un ciclo\n4)Mostrar estado general\n5)Buscar Mision\n6)Ranking de naves\n7)Salir");
             opcion = scanner.nextInt();
-
+            scanner.nextLine();
             switch (opcion) {
                 case 1:
-                   // registarMision();
+                  misiones.registrarMision();
                 break;
                 case 2:
                   //  registarNave();
@@ -53,9 +54,11 @@ public class Main {
                 case 7:
                     System.out.println("Adios!");
                 break;
-                
             }
         }while (opcion!=7);
+        //Logs para verificar el funcionamiento
+        misiones.logMisiones();
+
         scanner.close();
         }
 }
--- src/Mision.java
+++ src/Mision.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 import enums.ExperienciaTipo;
@@ -7,6 +9,7 @@ import enums.MissionType;
 public class Mision{
     Scanner scanner = new Scanner(System.in);
 
+    List<Mision> misiones = new ArrayList<>();
     private String nombre;
     private int duracion;
     private int prioridad;
@@ -83,19 +86,47 @@ public class Mision{
                 '}';
     }
 
-    public void registarMision(){
+    public void registrarMision(){
         System.out.println("Nombre: ");
         this.nombre = scanner.nextLine();
-        System.out.println("Tipo de mision(Exploración, Recolección de datos, Colonización): ");
+        System.out.println("Tipo de mision: 1)Exploración, 2)Recolección de datos, 3)Colonización): ");
         int opcion = scanner.nextInt();
-        if (opcion) {
-            
+        if (opcion == 1) {
+            this.tipo = MissionType.EXPLORACION;
+        }
+        else if(opcion ==2){
+            this.tipo = MissionType.RECOLECCION_DATOS;
+        }
+        else{
+            this.tipo = MissionType.COLONIZACION;
         }
         System.out.println("Duración: ");
         this.duracion = scanner.nextInt();
         System.out.println("Prioridad: ");
         this.prioridad = scanner.nextInt();
-        System.out.println("Experiencia requerida(Cientifica, Tecnica, Estrategica): ");
-        this.experienciaRequerida = ExperienciaTipo.valueOf(scanner.next().toUpperCase());
+        System.out.println("Experiencia requerida: 1)Cientifica, 2)Tecnica, 3)Estrategica): ");
+        int opcion2 = scanner.nextInt();
+        if(opcion2 == 1){
+            this.experienciaRequerida = ExperienciaTipo.CIENTIFICA;
+        }
+        else if(opcion2 == 2){
+            this.experienciaRequerida = ExperienciaTipo.TECNICA;
+        }
+        else{
+            this.experienciaRequerida = ExperienciaTipo.ESTRATEGICA;
+        }
+        Mision nuevamision = new Mision(nombre, opcion, opcion2, tipo, experienciaRequerida);
+        misiones.add(nuevamision);
+
+        System.out.println("\nMision añadida correctamente\n");
+    }
+
+    public void logMisiones(){
+        for (Mision misiones : this.misiones){
+            misiones.logMision();
+        }
+    }
+    public void logMision(){
+        System.out.println("Nombre: "+this.nombre+"\nTipo de mision: "+this.tipo+"\nDuración: "+this.duracion+"\nPrioridad: "+this.prioridad+"\nEstado: "+this.estado);
     }
 }
