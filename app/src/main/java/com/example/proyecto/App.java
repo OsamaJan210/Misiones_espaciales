187008aa3c2e9dec6ea1b70629543766656cc6d2
Marc-0207 <a17marlamver@inspedralbes.cat>
2025-06-10 10:25:21 +0200

    intento de arreglar imports

--- app/src/main/java/com/example/proyecto/App.java
+++ app/src/main/java/com/example/proyecto/App.java
@@ -1,11 +1,16 @@
 package com.example.proyecto;
 
-//import java.util.ArrayList;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.reflect.TypeToken;
+import java.lang.reflect.Type;
+import com.google.gson.Gson;
+
 
 public class App {
     public static void main(String[] args) {
@@ -154,21 +159,30 @@ public class App {
             }
     }
     private static void guardarDatos() {
-     // List<NavesEspaciales> naves = NavesEspaciales.getNaves();
+      List<NavesEspaciales> naves = NavesEspaciales.getNaves();
       List<Mision> misiones = Mision.getMisiones();
-      //List<App> misionNaves = new ArrayList<>();
       ObjectMapper mapper = new ObjectMapper();
 
       try{
-        File jsonFile = new File("Simulador.json");
-        mapper.writeValue(jsonFile, misiones);
-        System.out.println("Naves y misiones guardadas correctamente en Simulador.json");
-      } catch (IOException e){
+        File jsonMisiones = new File("Misiones.json");
+        mapper.writeValue(jsonMisiones, misiones);
+        File jsonNaves = new File("Naves.json");
+        mapper.writeValue(jsonNaves, naves);
+        System.out.println("Naves y misiones guardadas correctamente!");
+      } 
+      catch (IOException e){
         e.printStackTrace();
       }
     }
     private static void cargarDatos() {
-
+      try{
+        FileReader reader = new FileReader("Misiones.json");
+        Type listType = new TypeToken<List<Mision>>() {}.getType();
+        List<Mision> misiones = new Gson().fromJson(reader, listType);
+      } 
+      catch (Exception e) {
+            e.printStackTrace();
+      }
     }
 }
 
